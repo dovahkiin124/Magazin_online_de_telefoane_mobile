2eb2b6ad9ba8ede5890d2c0299c27c15e830badd
Ghidoarca Petru_Cristi <petru.ghidoarca@student.usv.ro>
2023-03-12 19:17:46 +0200

    Add files via upload
    Aplicatie de gestionare a unui magazin de telefoane mobile

---
+++ Class.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class Telefon
+{
+    public int Id { get; set; }
+    public string Brandul { get; set; }
+    public string Model { get; set; }
+    public decimal Pret { get; set; }
+    public string Descriere { get; set; }
+    public int Stoc { get; set; }
+
+    public Telefon(int id, string brand, string model, decimal pret, string descriere, int stoc)
+    {
+        Id = id;
+        Brandul = brand;
+        Model = model;
+        Pret = pret;
+        Descriere = descriere;
+        Stoc = stoc;
+    }
+}
+
+public class comandaprodus
+{
+    public Telefon Phone { get; set; }
+    public int Cantitate { get; set; }
+
+    public comandaprodus(Telefon phone, int quantity)
+    {
+        Phone = phone;
+        Cantitate = quantity;
+    }
+}
+
+public class Comanda
+{
+    private static int comandaId = 1;
+    public int Id { get; set; }
+    public List<comandaprodus> Produse { get; set; }
+    public decimal prettotal
+    {
+        get
+        {
+            return Produse.Sum(item => item.Phone.Pret * item.Cantitate);
+        }
+    }
+    public string Adresa{ get; set; }
+    public DateTime Data_comenzii { get; set; }
+    public bool Livrata { get; set; }
+
+    public Comanda(List<comandaprodus> produse, string adresa)
+    {
+        Id = comandaId++;
+        Produse = produse;
+        Adresa = adresa;
+        Data_comenzii = DateTime.Now;
+        Livrata = false;
+    }
+}
+
+public class Client
+{
+    private static int ClientId = 1;
+    public int Id { get; set; }
+    public string Nume { get; set; }
+    public string Email { get; set; }
+    public string Adresa { get; set; }
+    public List<Comanda> Comenzi { get; set; }
+
+    public Client(string nume, string email, string adresa)
+    {
+        Id = ClientId++;
+        Nume = nume;
+        Email = email;
+        Adresa = adresa;
+        Comenzi = new List<Comanda>();
+    }
+}
+
+public class MagazinulDeTelefoaneMobile
+{
+    private List<Telefon> telefoane = new List<Telefon>();
+    private List<Client> clienti = new List<Client>();
+    private List<Comanda> comenzi = new List<Comanda>();
+
+    public void Adauga_Telefon(Telefon telefon)
+    {
+        telefoane.Add(telefon);
+    }
+
+    public void Adauga_Client(Client client)
+    {
+        clienti.Add(client);
+    }
+
+    public void PlaceOrder(Client client, Comanda comanda)
+    {
+        if (!clienti.Contains(client))
+        {
+            clienti.Add(client);
+        }
+        client.Comenzi.Add(comanda);
+        comenzi.Add(comanda);
+        foreach (var produs in comanda.Produse)
+        {
+            produs.Phone.Stoc -= produs.Cantitate;
+        }
+    }
+
+    public List<Telefon> GetTelefoane()
+    {
+        return telefoane;
+    }
+
+    public List<Client> GetClienti()
+    {
+        return clienti;
+    }
+
+    public List<Comanda> GetComenzi()
+    {
+        return comenzi;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MagazinulDeTelefoaneMobile shop = new MagazinulDeTelefoaneMobile();
+
+        // Adding phones to the shop
+        Telefon phone1 = new Telefon(1, "Apple", "iPhone 12", 799.99m, "The iPhone 12 features a 6.1-inch Super Retina XDR display and A14 Bionic chip.", 10);
+        Telefon phone2 = new Telefon(2, "Samsung", "Galaxy S21", 699.99m, "The Galaxy S21 features a 6.2-inch Dynamic AMOLED 2X display and Exynos 2100 chip.", 8);
+        Telefon phone3 = new Telefon(3, "Google", "Pixel 5", 699.99m, "The Pixel 5 features a 6-inch OLED display and Snapdragon 765G chip.", 5);
+        shop.Adauga_Telefon(phone1);
+        shop.Adauga_Telefon(phone2);
+        shop.Adauga_Telefon(phone3);
+
+        // Adding customers to the shop
+        Client customer1 = new Client("John Smith", "john.smith@example.com", "123 Main St, Anytown USA");
+        Client customer2 = new Client("Jane Doe", "jane.doe@example.com", "456 Oak Ave, Anytown USA");
+        shop.Adauga_Client(customer1);
+        shop.Adauga_Client(customer2);
+
+        // Placing an order
+        comandaprodus orderItem1 = new comandaprodus(phone1, 2);
+        comandaprodus orderItem2 = new comandaprodus(phone2, 1);
+        Comanda order = new Comanda(new List<comandaprodus> { orderItem1, orderItem2 }, "789 Elm St, Anytown USA");
+        shop.PlaceOrder(customer1, order);
+
+        // Displaying order details
+        Console.WriteLine($"ID-ul comenzii: {order.Id}");
+        Console.WriteLine($"Data comenzii: {order.Data_comenzii}");
+        Console.WriteLine($"Adresa comenzii: {order.Adresa}");
+        Console.WriteLine("Produse comandate:");
+        foreach (var item in order.Produse)
+        {
+            Console.WriteLine($"{item.Phone.Brandul} {item.Phone.Model} x {item.Cantitate} - ${item.Phone.Pret}");
+        }
+        Console.WriteLine($"Pret Total: ${order.prettotal}");
+        Console.WriteLine($"Comanda Livrata: {order.Livrata}");
+
+        // Displaying customer order history
+        Console.WriteLine("Comenzi anterioare ale clientului:");
+        foreach (var customerOrder in customer1.Comenzi)
+        {
+            Console.WriteLine($"ID-ul comenzii: {customerOrder.Id}, Data comenzii: {customerOrder.Data_comenzii}, Pret Total: ${customerOrder.Adresa}");
+        }
+    }
+}
